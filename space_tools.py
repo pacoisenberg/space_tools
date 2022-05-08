c587f343292a2caafc9288160254e64c882846ce
paco.isenberg <paco.isenberg@gmail.com>
2022-05-08 16:07:05 -0700

    allow choice of calculating azimuth or inclination

--- space_tools.py
+++ space_tools.py
@@ -1,11 +1,11 @@
 import math
 
 spaceport_lats = {
-    'PSCA': 57,
-    'SLC-46': 29,
-    'Camden': 33,
-    'Vandenberg': 31,
-    'SaxaVord': 68
+    'PSCA': 57.4304338,
+    'SLC46': 28.4581376,
+    'Camden': 30.9366602,
+    'SLC8': 34.5763024,
+    'SaxaVord': 60.8190531
 }
 
 def inc_from_az(lat,azimuth):
@@ -16,7 +16,7 @@ def inc_from_az(lat,azimuth):
         math.acos(
             math.cos(rlat)*math.sin(razimuth)
             )
-        ),1)
+        ),2)
 
     return(inc)
 
@@ -29,41 +29,53 @@ def az_from_inc(lat,inclination):
                 math.asin(
                     math.cos(rinc)/math.cos(rlat)
                 )
-            ),1)
+            ),2)
         return(az)
     except ValueError:
         return(None)
 
-spaceport = ""
-while spaceport not in spaceport_lats:
-    for key in spaceport_lats:
-        print(key)
+def inc_or_az():
+    calc_choice = ""
+    while calc_choice not in ['a','i']:
+        calc_choice = input("Do you want to calculate (a)zimuth or (i)nclination?" )
+        calc_choice = calc_choice[0]
+    return (calc_choice)
 
-    spaceport = input("Type the name of a spaceport from the list to launch from: ")
+def lat_user_choice(spaceport_dictionary):
 
-    if spaceport == "quit":
-        break
-    try:
-        lat = spaceport_lats[spaceport]
-        print(f"The latitude of {spaceport} is {lat} degrees")
-    except KeyError:
-        print(f"{spaceport} is not a spaceport")
+    spaceport = ""
+    while spaceport not in spaceport_lats:
+        for key in spaceport_lats:
+            print(key)
 
+        spaceport = input("Type the name of a spaceport from the list or enter a latitude to launch from: ")
 
-# azimuth = float(input("What azimuth are you flying? "))
-# dinc = inc_from_az(lat, azimuth)
-# print(f"At latitude {lat}, an azimuth of {azimuth}, results in the inclination of: {dinc}")
-#
-inclination = float(input("What inclination are you trying to hit? "))
-daz = az_from_inc(lat, inclination)
-print(f"At latitude {lat} ({spaceport}), to achieve an inclination of {inclination}, the azimuth required is: {daz}")
+        if spaceport == "quit":
+            break
+        try:
+            lat = spaceport_lats[spaceport]
+            print(f"The latitude of {spaceport} is {lat} degrees")
+        except KeyError:
+            try:
+                lat = float(spaceport)
+                if 0 <= lat <= 90:
+                    break
+                else:
+                    raise ValueError
+            except ValueError:
+                print(f"{spaceport} is not a valid entry")
 
+    return(spaceport, lat)
 
+spaceport, lat = lat_user_choice(spaceport_lats)
+type_of_calc = inc_or_az()
 
-# for azimuth in range(360):
-#     dinc = inc_from_az(lat, azimuth)
-#     print(f"At latitude {lat}, an azimuth of {azimuth}, results in the inclination of: {dinc}")
-#
-# for inclination in range(360):
-#     daz = az_from_inc(lat, inclination)
-#     print(f"At latitude {lat}, to achieve an inclination of {inclination}, the azimuth required is: {daz}")
+if type_of_calc == "i":
+    inclination = float(input("What inclination are you trying to hit? "))
+    daz = az_from_inc(lat, inclination)
+    print(f"At latitude {lat} ({spaceport}), to achieve an inclination of {inclination}, the azimuth required is: {daz}")
+
+elif type_of_calc == "a":
+    azimuth = float(input("What azimuth are you flying? "))
+    dinc = inc_from_az(lat, azimuth)
+    print(f"At latitude {lat} ({spaceport}), an azimuth of {azimuth}, results in the inclination of: {dinc}")
